70820270edcdcc7c85d5c6270126213b996df114
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-09-01 02:13:06 +0300

    init

---
+++ server/build/server.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const config_1 = require("./config/config");
+const Logging_1 = __importDefault(require("./library/Logging"));
+const cors_1 = __importDefault(require("cors"));
+const router = (0, express_1.default)();
+/** Use cors middlware */
+router.use((0, cors_1.default)());
+/** Start the server */
+const StartServer = () => {
+    router.use((req, res, next) => {
+        /** Log the Request */
+        Logging_1.default.info(`Incoming -> Method: [${req.method} - Url: ${req.url}] - IP: [${req.socket.remoteAddress}]`);
+        res.on("finish", () => {
+            /** Log the Response */
+            Logging_1.default.info(`Incoming -> Method: [${req.method} - Url: ${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        });
+        next();
+    });
+    router.use(express_1.default.urlencoded({ extended: true }));
+    router.use(express_1.default.json());
+    /** Rules of our API */
+    router.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "Origin, X-Requested-with, Content-Type, Accept, Authorization");
+        if (req.method == "OPTIONS") {
+            res.header("Access-Control-Allow-Origin", "PUT, POST, PATCH, DELETE, GET");
+            return res.status(200).json({});
+        }
+        next();
+    });
+    /** Routes */
+    /** Healthcheck */
+    router.get("/ping", (req, res, next) => res.status(200).json({ message: "pong" }));
+    /** Error handling */
+    router.use((req, res, next) => {
+        const error = new Error("not found");
+        Logging_1.default.error(error);
+        return res.status(404).json({ message: error.message });
+    });
+    http_1.default.createServer(router).listen(config_1.config.server.port, () => Logging_1.default.info(`Server listening on port ${config_1.config.server.port}.`));
+};
+Logging_1.default.info("Connection established");
+StartServer();
---
+++ server/src/server.ts
@@ -0,0 +1,58 @@
+import express from "express"
+import http from "http"
+import { config } from "./config/config"
+import Logging from "./library/Logging"
+import cors from "cors"
+
+const router = express()
+
+/** Use cors middlware */
+router.use(cors())
+
+/** Start the server */
+const StartServer = () => {
+	router.use((req, res, next) => {
+		/** Log the Request */
+		Logging.info(`Incoming -> Method: [${req.method} - Url: ${req.url}] - IP: [${req.socket.remoteAddress}]`)
+		res.on("finish", () => {
+			/** Log the Response */
+			Logging.info(`Incoming -> Method: [${req.method} - Url: ${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`)
+		})
+
+		next()
+	})
+	router.use(express.urlencoded({ extended: true }))
+	router.use(express.json())
+
+	/** Rules of our API */
+	router.use((req, res, next) => {
+		res.header("Access-Control-Allow-Origin", "*")
+		res.header("Access-Control-Allow-Methods", "Origin, X-Requested-with, Content-Type, Accept, Authorization")
+
+		if (req.method == "OPTIONS") {
+			res.header("Access-Control-Allow-Origin", "PUT, POST, PATCH, DELETE, GET")
+			return res.status(200).json({})
+		}
+
+		next()
+	})
+
+	/** Routes */
+
+	/** Healthcheck */
+
+	router.get("/ping", (req, res, next) => res.status(200).json({ message: "pong" }))
+
+	/** Error handling */
+	router.use((req, res, next) => {
+		const error = new Error("not found")
+		Logging.error(error)
+
+		return res.status(404).json({ message: error.message })
+	})
+
+	http.createServer(router).listen(config.server.port, () => Logging.info(`Server listening on port ${config.server.port}`))
+}
+
+Logging.info("Connection established")
+StartServer()
