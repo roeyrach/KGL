0a3f4225aad7304de016238eb5d71715011d05a7
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-09-10 13:37:46 +0300

    pagination

--- client/src/components/GenericList/GenericList.js
+++ client/src/components/GenericList/GenericList.js
@@ -6,6 +6,8 @@ import SearchBar from "../SearchBar/SearchBar"
 function GenericList({ Component, fetch, ...args }) {
 	const [originalList, setOriginalList] = useState([]) // Store the original list
 	const [list, setList] = useState([]) // Use this for filtering
+	const [currentPage, setCurrentPage] = useState(1)
+	const itemsPerPage = 10 // Number of items to display per page
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -27,8 +29,25 @@ function GenericList({ Component, fetch, ...args }) {
 	}, [fetch])
 
 	const renderedList = useMemo(() => {
-		return list.map((item) => <Component className="generic-list-item" key={uuidv4()} itemInfo={item} />)
-	}, [list])
+		const startIndex = (currentPage - 1) * itemsPerPage
+		const endIndex = startIndex + itemsPerPage
+		const itemToDisplay = list.slice(startIndex, endIndex)
+
+		return itemToDisplay.map((item) => <Component className="generic-list-item" key={uuidv4()} itemInfo={item} />)
+	}, [list, currentPage])
+
+	const goToPage = (page) => {
+		setCurrentPage(page)
+	}
+
+	const goToPreviousPage = () => {
+		if (currentPage > 1) goToPage(currentPage - 1)
+	}
+
+	const goToNextPage = () => {
+		const totalPages = Math.ceil(list.length / itemsPerPage)
+		if (currentPage < totalPages) goToPage(currentPage + 1)
+	}
 
 	const handleSearch = (event) => {
 		const searchTerm = event.target.value
@@ -39,9 +58,22 @@ function GenericList({ Component, fetch, ...args }) {
 		setList(filteredList)
 	}
 
+	const totalPages = Math.ceil(list.length / itemsPerPage)
+
 	return (
 		<div className="body">
 			<SearchBar handleSearch={handleSearch}></SearchBar>
+			<div className="pagination">
+				<button className="previes-button" onClick={goToPreviousPage} disabled={currentPage === 1}>
+					Previes
+				</button>
+				<span>
+					Page {currentPage} of {totalPages}
+				</span>
+				<button className="next-button" onClick={goToNextPage} disabled={currentPage === totalPages}>
+					Next
+				</button>
+			</div>
 			<div className="generic-list-container">{renderedList}</div>
 		</div>
 	)
